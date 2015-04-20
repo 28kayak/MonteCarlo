332581011c0d56711a64f5a53e7cdcdb5b210a0b
28kayak <kaya_ota@msn.com>
2015-04-19 23:06:42 -0700

    Monte Flip Coin

---
+++ MonteCarlo.R
@@ -0,0 +1,38 @@
+monte <- function(n) 
+{
+	count <- 0 
+	#initilize variable named count that count 
+	#how many random number gives in the range of  1 > 0.5 
+	for (i in 1:n)
+	{# Repeat n times 
+		if(runif(1)>0.5) 
+		{# if the rundom number given by the function funif() in the range of  1 > 0.5 
+		 #then increment count by one
+			count <- count +1 
+		}
+	}
+	#devide counter by total number of random number we pick
+	return (count/n)
+
+}
+plotMonte <- function(n)
+{
+	count <- c()
+	#define array(or vector) named count
+	for (i in 1:n)
+	{
+		count <- c(count, monte(i))
+		#stack a pair of count and result from monte function in to count 
+	}
+	plot(count, type = "l")
+	#pot the result 
+	par(new = T)
+	#allow rewrite plot
+}
+
+monte(10000)
+#call monte function with n = 10000
+plotMonte(10000)
+#call plotMonte function with n = 10000
+abline(h = 0.5, col = "red")
+#drow a red line with hight = 0.5
